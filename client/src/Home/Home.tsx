a69e28113542027a4ad4784f88056072ceb2c107
eyalalon123 <eyalalon2000@gmail.com>
2024-06-04 13:11:14 +0300

    test

--- client/src/Home/Home.tsx
+++ client/src/Home/Home.tsx
@@ -1,22 +1,41 @@
 import axios from "axios";
+import { useState } from "react";
 
+import "./home.scss"
+
+interface users {
+    name: string;
+    _id: string
+    phoneNumber: string;
+}
 const Home = () => {
+    const [users, setUsers] = useState<users[]>([]);
+    const [loading, setLoading] = useState(false);
 
     async function handleUsers() {
+        setLoading(true);
         try {
             const response = await axios.get("http://localhost:8000/users");
-            console.log(response.data);
+            setUsers(response.data);
         } catch (error) {
             console.error("Error fetching users:", error);
+        } finally {
+            setLoading(false);
         }
     }
 
     return (
-        <>
-            <div>
-                <button onClick={handleUsers}>Fetch Users</button>
+        <div className="home-page-container">
+            <button className="button-handle-users" onClick={handleUsers}>Fetch Users</button>
+            <div className="data-container">
+                {loading && <p>Loading...</p>}
+                <div>
+                    {users.map((user) => (
+                        <p key={user._id}>{user.name} + {user.phoneNumber}</p>
+                    ))}
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
--- server/src/users/schemas/user.shcema.ts
+++ server/src/users/schemas/user.shcema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
-@Schema()
+@Schema({ versionKey: false })
 export class Users {
 
     @Prop({ type: String, required: true })
